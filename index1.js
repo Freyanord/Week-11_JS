2371780bc52bf9d562f2b1ed4c3847a8e9c88cfc
Freyanord <annabashina135@gmail.com>
2023-05-18 21:13:17 +0300

    Correct

--- index1.js
+++ index1.js
@@ -1,1 +1,14 @@
 //задание под *
+const numberCount = document.querySelector('.number_count');
+const button = document.querySelector('button');
+
+function calc () {
+    let numberAdd = document.querySelector('.number_add').value;
+    let result = numberAdd * numberAdd;
+    numberCount.value = result;
+}
+
+button.addEventListener('click', calc);
+
+
+
